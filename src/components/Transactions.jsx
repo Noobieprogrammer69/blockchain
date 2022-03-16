7293565a65eb7a9ba8c96465ce3175f84f8fdcba
Noobieprogrammer69 <joververgara1@gmail.com>
2022-03-16 16:19:41 +0800

    some errorr

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -1,7 +1,6 @@
 import React, { useContext } from 'react';
 import { TransactionContext } from '../context/TransactionContext';
 
-import dummydata from '../utils/dummydata';
 import { shortenAddress } from '../utils/shortenAddress';
 import useFetch from '../hooks/useFetch';
 
